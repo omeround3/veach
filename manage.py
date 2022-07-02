1105a839a5e87dd48704a4ea540f19421f49a015
omer-lev-ron <omer@ametos.com>
2022-07-02 14:29:57 +0000

    Fixed settings module

--- manage.py
+++ manage.py
@@ -6,8 +6,7 @@ import core.analyser.rules_generator
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'veach.settings.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'veach.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
